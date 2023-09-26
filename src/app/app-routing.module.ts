cdf8a6897cee4dfd8e81ee9f66cab88ecd0eaa3a
MichalPajestka <001michal002@gmail.com>
2023-09-26 13:03:49 +0200

    Update routing -Added paths for the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPage } from './pages/landing/landing.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LandingPage
+  },
+  {
+    path: "trainer",
+    component: TrainerPage
+  },
+  {
+    path: "pokemon",
+    component: PokemonCataloguePage
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LandingPage } from './pages/landing/landing.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPage,
+    TrainerPage,
+    PokemonCataloguePage
   ],
   imports: [
     BrowserModule,
