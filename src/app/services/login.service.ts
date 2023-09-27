5876d44aadd7ca4279598072f909baeffa47fab2
Michal Pajestka <001michal002@gmail.com>
2023-09-27 09:48:00 +0200

    Create login service

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { }
+
+  //TODO: Login
+
+  //Check if user exists
+
+  //If NOT user - Create a user
+
+  // IF user - store user
+
+  // IF user || Created user -> store user
+}
