8c0e34920fabb0a658bcf4275feae84d064c1c0b
Michal Pajestka <001michal002@gmail.com>
2023-09-28 22:58:08 +0200

    create user.model.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs'; // Import BehaviorSubject
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+
+import { User } from '../models/user.model'; // Import the User model
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private apiUrl = environment.apiTrainers;
   private apiKey = environment.apiKey;
+
+  // Use BehaviorSubject to store and provide the user data
+  private userSubject = new BehaviorSubject<User | null>(null);
+  user$: Observable<User | null> = this.userSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -19,15 +26,32 @@ export class UserService {
 
   userExists(username: string): Observable<boolean> {
     // Check if a user with the given username already exists
-    return this.http.get<any[]>(`${this.apiUrl}?username=${username}`, { headers: this.getHeaders() })
-      .pipe(
-        map(users => users.length > 0)
-      );
+    return this.http
+      .get<any[]>(`${this.apiUrl}?username=${username}`, {
+        headers: this.getHeaders(),
+      })
+      .pipe(map((users) => users.length > 0));
   }
 
   addUser(username: string): Observable<any> {
     // Create a new user and add it to the API
-    const newUser = { username, pokemon: [] };
-    return this.http.post<any>(this.apiUrl, newUser, { headers: this.getHeaders() });
+    const newUser: User = { id: 0, username, pokemon: [] }; // Match the User interface
+    return this.http
+      .post<any>(this.apiUrl, newUser, { headers: this.getHeaders() })
+      .pipe(
+        // Update user data in the BehaviorSubject after a successful login
+        map(() => {
+          this.userSubject.next(newUser); // Pass the newUser object
+        })
+      );
+  }
+
+  // Add a method to get the current user data
+  getCurrentUser(): User | null {
+    return this.userSubject.value;
+  }
+
+  updateUser(user: User | null) {
+    this.userSubject.next(user);
   }
 }
