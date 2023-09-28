a481b9235efca537ddd2b623f8288971e0cf8897
Michal Pajestka <001michal002@gmail.com>
2023-09-28 21:39:54 +0200

    Update guard code

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,28 @@
 import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  // Check if a Trainer name exists in local or session storage
-  const username = localStorage.getItem('username') || sessionStorage.getItem('username');
-
-  if (username) {
-    // Trainer name exists, allow access to the route
-    return true;
-  } else {
-    // No Trainer name found, redirect to the landing page
-    window.location.href = '/landing'; // Use window.location.href to navigate
-    return false;
-  }
+  return true;
 };
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    // Check if a Trainer name exists in local or session storage
+    const username = localStorage.getItem('username') || sessionStorage.getItem('username');
+
+    if (username) {
+      // Trainer name exists, allow access to the route
+      return true;
+    } else {
+      // No Trainer name found, navigate to the landing page
+      this.router.navigate(['/landing']);
+      return false;
+    }
+  }
+}
