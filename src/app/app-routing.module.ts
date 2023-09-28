8444134f0a77710cf168b3091003ec85051ed4d0
MichalPajestka <001michal002@gmail.com>
2023-09-28 14:27:01 +0200

    Refactor app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,11 +18,11 @@ const routes: Routes = [
     path: "trainer",
     component: TrainerPage
   },
-  {
-    path: "pokemon",
+  { 
+    path: 'pokemon', 
     component: PokemonCataloguePage
-  }
-];
+  },
+]
 
 @NgModule({
   imports: [
