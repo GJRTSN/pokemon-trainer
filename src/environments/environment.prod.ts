f7f83cc6be4f71a5218ce8da7b4b6911dded28c0
Michal Pajestka <001michal002@gmail.com>
2023-09-28 19:14:20 +0200

    Implement environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiTrainers: "https://trainers-api-production-3e9d.up.railway.app/trainers",
+    apiKey: 'xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o'
+};
