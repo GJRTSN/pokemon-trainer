2876a75c9412ada20133c4bbc7be702446c15e0e
Michal Pajestka <001michal002@gmail.com>
2023-09-28 22:58:20 +0200

    update login-form.component.html

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service'; // Import UserService
+import { User } from '../../models/user.model'; // Import User model
 
 @Component({
   selector: 'app-login-form',
@@ -18,7 +19,7 @@ export class LoginFormComponent {
     // Reset error flags
     this.showRequiredError = false;
     this.showMinLengthError = false;
-  
+
     if (this.username.length === 0) {
       this.showRequiredError = true;
     } else if (this.username.length < 2) {
@@ -28,6 +29,10 @@ export class LoginFormComponent {
       this.userService.userExists(this.username).subscribe((exists) => {
         console.log('User exists:', exists);
         if (exists) {
+          console.log('Logging in existing user...');
+          // Get the user data and update it
+          const user: User = { id: 0, username: this.username, pokemon: [] };
+          this.userService.updateUser(user);
           console.log('Navigating to /pokemon...');
           this.router.navigate(['/pokemon']);
         } else {
@@ -35,7 +40,7 @@ export class LoginFormComponent {
           this.userService.addUser(this.username).subscribe(() => {
             // Save the username in local storage
             localStorage.setItem('username', this.username);
-          
+
             // Redirect to the catalog page
             this.router.navigate(['/pokemon']);
           });
@@ -43,5 +48,4 @@ export class LoginFormComponent {
       });
     }
   }
-  
 }
